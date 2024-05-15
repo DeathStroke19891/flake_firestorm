618a326038285ab15e632b4f31b5c0b210c4e1d9
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-05-15 18:40:06 +0530

    Add log support

--- nixos/derivations/rebuild.nix
+++ nixos/derivations/rebuild.nix
@@ -6,7 +6,7 @@ pkgs.writeShellScriptBin "rebuild" ''
   ${pkgs.alejandra}/bin/alejandra . &>/dev/null
   ${pkgs.git}/bin/git diff -U0 *.nix
   echo "NixOS Rebuilding..."
-  sudo nixos-rebuild switch --flake ~/flake_firestorm/ &>nixos-switch.log || ( cat nixos-switch.log | grep --color error && false)
+  sudo nixos-rebuild switch --flake ~/flake_firestorm/ &>~/flake_firestorm/nixos/nixos-switch.log || ( cat nixos-switch.log | grep --color error && false)
   gen=$(nixos-rebuild list-generations | grep current)
   ${pkgs.git}/bin/git commit -am "$gen"
 ''
