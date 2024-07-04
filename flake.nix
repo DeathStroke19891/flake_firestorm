393d02dbc32223ea7e14254e9509994ab280c657
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-07-04 08:37:18 +0530

    Add nix-alien inputs

--- flake.nix
+++ flake.nix
@@ -12,6 +12,8 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-alien.url = "github:thiagokokada/nix-alien";
   };
 
   outputs = {
