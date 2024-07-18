c8658bfbd720629453379b8c0a5b498ce55be65b
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-07-18 22:55:22 +0530

    75 current  2024-07-18 22:55:07  24.11.20240714.693bc46  6.6.39                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -209,7 +209,7 @@
   console.keyMap = "dvorak-programmer";
   hardware.uinput.enable = true;
 
-  sound.enable = true;
+  # sound.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
