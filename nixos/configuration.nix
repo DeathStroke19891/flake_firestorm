d2b1ab927c6218d2851ba6dd409635a81fbe483f
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-09-14 23:04:55 +0530

    22 current  2024-09-14 23:04:39  24.11.20240910.1355a0c  6.10.7-zen1                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -71,6 +71,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
   boot.kernelPackages = pkgs.linuxPackages_zen;
