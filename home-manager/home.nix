3288c120333c3b8709a4e5a8ae7c0d5e1000ed17
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-10-02 09:38:05 +0530

    2024-10-02 09:38 : id 50 -> /nix/store/zw45pk5gbagn8p3038lc9hjlvdnmc1mn-home-manager-generation

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -144,14 +144,10 @@
         {
           'neovim/nvim-lspconfig',
           config = function()
-            local on_attach = require("plugins.configs.lspconfig").on_attach
-            local capabilities = require("plugins.configs.lspconfig").capabilities
 
             local lspconfig = require "lspconfig"
 
             lspconfig.rust_analyzer.setup{
-              on_attach = on_attach,
-              capabilities = capabilities,
               filetypes = {"rust"},
               root_dir = lspconfig.util.root_pattern("Cargo.toml"),
               settings = {
@@ -164,8 +160,6 @@
             }
 
             lspconfig.pylsp.setup{
-              on_attach = on_attach,
-              capabilities = capabilities,
               filetypes = {"python"},
               settings = {
                 pylsp = {
