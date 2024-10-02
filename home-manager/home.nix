234def82a8b62d7cca6feac24cf48a19b149f1a7
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-10-02 10:45:42 +0530

    2024-10-02 10:45 : id 55 -> /nix/store/fq5chjw2br7n2gj9bl1105zrx61bxcs9-home-manager-generation

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -186,12 +186,26 @@
           event = "VeryLazy",
           opts = function()
             local null_ls = require("null-ls")
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
             local opts = {
               sources = {
                 null_ls.builtins.formatting.clang_format,
               }
+              on_attach = function(client, bufnr)
+                  if client.supports_method("textDocument/formatting") then
+                      vim.api.nvim_clear_autocmds({
+                        group = augroup,
+                        buffer = bufnr
+                      })
+                      vim.api.nvim_create_autocmd("BufWritePre", {
+                          group = augroup,
+                          buffer = bufnr,
+                          callback = vim.lsp.buf.format({ async = false }),
+                      })
+                  end
             }
+
             return opts
           end,
         },
