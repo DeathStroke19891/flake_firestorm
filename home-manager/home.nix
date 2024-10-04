775a216a0c3bd3eeb22f9835c9d0eba3cc1a4071
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-10-04 06:45:57 +0530

    2024-10-04 06:45 : id 70 -> /nix/store/203w47201qn8vfyv4r42khhxhicaqhkg-home-manager-generation

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -268,6 +268,22 @@
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
                 ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                                local status_ok, luasnip = pcall(require, "luasnip")
+                                if status_ok and luasnip.expand_or_jumpable() then
+                                    luasnip.expand_or_jump()
+                                else
+                                    fallback()
+                                end
+                            end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    local status_ok, luasnip = pcall(require, "luasnip")
+                    if status_ok and luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" })
               }),
               sources = cmp.config.sources({
                 -- { name = 'nvim_lsp' },
