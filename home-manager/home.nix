ab50126eec330c3c6a9c15378afc0784213f4027
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-10-04 07:50:01 +0530

    2024-10-04 07:49 : id 74 -> /nix/store/wx7k4v9kn3ynfx3rxzvqbivcbdiwiddv-home-manager-generation

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -317,12 +317,12 @@
             end
 
             local function footer()
-                local total_plugins = #vim.tbl_keys(packer_plugins)
+                -- local total_plugins = #vim.tbl_keys(packer_plugins)
                 local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
                 local version = vim.version()
                 local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
 
-                return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
+                return datetime .. "   " .. nvim_version_info
             end
 
             local logo = {
