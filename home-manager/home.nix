54dad483de91598840dd7996b3a653d15a1c5cb9
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-10-04 06:33:39 +0530

    2024-10-04 06:33 : id 69 -> /nix/store/ydgpfws4zqbp7zyl3lcagg6v7jc0qq2h-home-manager-generation

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -146,7 +146,7 @@
           config = function()
 
             local lspconfig = require "lspconfig"
-            local capabilities = require('cmp_nvim_lsp').default_capabilities
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
             lspconfig.rust_analyzer.setup{
               capabilities = capabilities,
