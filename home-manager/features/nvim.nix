52844791f80795c6dcd7860460e27446e60e1538
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-10-28 22:37:29 +0530

    2024-10-28 22:37 : id 97 -> /nix/store/cm4y419snnb56v4xri8mf9gbqndj6ajb-home-manager-generation

--- home-manager/features/nvim.nix
+++ home-manager/features/nvim.nix
@@ -16,7 +16,7 @@
         {
           "davidmh/mdx.nvim",
           config = true,
-          lazy = false,
+          event = VeryLazy,
           dependencies = {"nvim-treesitter/nvim-treesitter"}
         },
         {
