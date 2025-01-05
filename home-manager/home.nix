b901639850ebb6ea7bbe846537c9b80e8c68d1ea
Sridhar D Kedlaya <sridhardked@gmail.com>
2025-01-05 07:58:34 +0530

    2025-01-05 07:58 : id 18 -> /nix/store/1vzj2yznw89h7dgp4hmq2imwrrw7ax8y-home-manager-generation

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -287,6 +287,8 @@
     nerd-fonts.monaspace
     nerd-fonts.mononoki
 
+    anki-bin
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
