30336b6f064e9e19d7f8efc91a9b928d068bafd1
Sridhar D Kedlaya <sridhardked@gmail.com>
2025-01-05 07:40:22 +0530

    2025-01-05 07:40 : id 15 -> /nix/store/144v15i2w97pi6kvh0q1gfhk04y8h32w-home-manager-generation

--- home-manager/features/nvim.nix
+++ home-manager/features/nvim.nix
@@ -252,8 +252,12 @@
         },
         {
           url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+          event = "VeryLazy"
           config = function()
             require("lsp_lines").setup()
+            vim.diagnostic.config({
+              virtual_text = false,
+            })
           end,
         }
       }
