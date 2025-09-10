88dbe0784bc43f6698e710c20c3686d85c0030cf
Sridhar D Kedlaya <sridhardked@gmail.com>
2025-09-10 08:13:06 +0530

    2025-09-10 08:13 : id 88 -> /nix/store/jjmz2sf3llhwpw27jd43pr439mg0ami2-home-manager-generation (current)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -64,7 +64,7 @@
         {
           name = "main remaps";
           remap = {
-            "rightalt" = "rightmeta";
+            "rightalt" = "esc";
             CapsLock = {
               held = "leftctrl";
               alone = "esc";
