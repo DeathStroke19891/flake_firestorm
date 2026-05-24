e12567d4833fdca0e8557d8ef3ca709bce3ed113
Sridhar D Kedlaya <sridhar.dkedlaya@arcticwolf.com>
2026-05-24 13:34:20 +0530

    Add fixes

--- wrappedPrograms/environment.nix
+++ wrappedPrograms/environment.nix
@@ -1,21 +1,22 @@
-{self, ...}: {
+{
+  inputs,
+  self,
+  ...
+}: {
   perSystem = {
     pkgs,
     self',
     lib,
     ...
   }: {
-    packages.environment = pkgs.runCommand "environment" {
-      nativeBuildInputs = [pkgs.makeWrapper];
-      meta.mainProgram = "zsh";
-    } ''
-      mkdir -p $out/bin
-      makeWrapper ${self'.packages.zsh}/bin/zsh $out/bin/zsh \
-        --prefix PATH : ${lib.makeBinPath [
-          self'.packages.cli-tools
-          self'.packages.dev-tools
-          self'.packages.git
-        ]}
-    '';
+    packages.environment = inputs.wrapper-modules.lib.wrapPackage {
+      inherit pkgs;
+      package = self'.packages.zsh;
+      runtimeInputs = [
+        self'.packages.cli-tools
+        self'.packages.dev-tools
+        self'.packages.git
+      ];
+    };
   };
 }
